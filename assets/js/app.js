0abcbf24324ed383451127e1c217237dfea3eb33
Weng Fei Fung <weffung@ucdavis.edu>
2022-02-10 20:01:29 -0800

    Fixed begin time validation; Working engine for parsing task and time fractions; Placeholder instructions

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,6 +13,20 @@ let utilities = {
         } // for
         return isNum;
     }, 
+    checkIsFloatOrInt: (str) => {
+        str = "" + str;
+        if(str.length===0) return false;
+
+        let isFloat = true;
+        for(let i = 0; i<str.length; i++) {
+            if(str[i]===".") {
+            } else if(str[i]>'9' || str[i]<'0') {
+                isFloat=false;
+                break;
+            }
+        } // for
+        return isFloat;
+    }, 
     cvtMilitaryTimeToFractional: (militaryTime) => {
         let h = parseInt(militaryTime.substr(0,2));
         let m = parseInt(militaryTime.substr(2,2));
@@ -39,18 +53,19 @@ let app = {
     $userInputs:"",
     $timeSuggestions:"",
     currentTime:"",
+    tasks: [],
 
     init: function($doms) {
         Object.assign(app, $doms);
         this.currentTime = "" + (new Date).getHours() + (new Date).getMinutes();
 
-        // User sets
+        // User sets begin time
         this.$begin.on("input", ev=>{
             const {target:el} = ev;
             const $el = $(el);
 
             $el.removeClass("is-valid").removeClass("is-invalid");
-            if($el.val.length==4 && utilities.checkAllNumbers(val)) {
+            if($el.val().length==4 && utilities.checkAllNumbers($el.val())) {
                 $el.addClass("is-valid");
                 this.currentTime = $el.val;
             } else {
@@ -58,5 +73,53 @@ let app = {
                 this.currentTime = "" + (new Date).getHours() + (new Date).getMinutes();
             }
         });
+
+        // User sets tasks
+        this.$userInputs.on("input", ev=>{
+            const {target:el} = ev;
+            const $el = $(el);
+
+            app.tasks = []; // reset tasks
+            let lines = $el.val().split("\n");
+            // console.log({linesLength2:lines.length==2})
+            for(let i = 0; i<lines.length; i++) {
+                let line = lines[i];
+                let parsed = line.split(/[\t\s]/);
+                let parsedRight = parsed[ parsed.length-1 ];
+                if(utilities.checkIsFloatOrInt(parsedRight)) {
+                    this.tasks.push({
+                        task: parsed.slice(0,-1).join(" "),
+                        militaryTime: (function() {
+                            let lineTimeFraction = parseFloat(parsedRight);
+                            let currentTimeFraction = utilities.cvtMilitaryTimeToFractional(app.currentTime);
+                            let futureTimeFraction = currentTimeFraction + lineTimeFraction;
+                            let futureTimeMilitary = utilities.cvtFractionalToMilitaryTime(futureTimeFraction);
+                            // console.log({futureTimeFraction, should:"Int.Dec"})
+                            // console.log({futureTimeMilitary, should:"HHMM"})
+                            return futureTimeMilitary
+                        })()
+                    })
+                } // vald line
+            } // check all lines
+
+            const $suggestions = this.$timeSuggestions.find("ul");
+            $suggestions.html(""); // Reset suggestions
+            app.tasks = app.tasks.sort((taskA, taskB)=>{
+                let fractionA = utilities.cvtMilitaryTimeToFractional(taskA.militaryTime);
+                let fractionB = utilities.cvtMilitaryTimeToFractional(taskB.militaryTime);
+                return fractionA - fractionB;
+            });
+            console.log({tasksSorted: app.tasks})
+
+            for(let i = 0; i<app.tasks.length; i++) {
+                let task = app.tasks[i];
+                $suggestions.append(`
+                    <li class="suggestion">
+                        <span class="suggestion-time">${task.militaryTime}</span>
+                        <span class="suggestion-task">${task.task}</span>
+                    </li>
+                `);
+            }
+        }) // on user inputting tasks
     }
 }
