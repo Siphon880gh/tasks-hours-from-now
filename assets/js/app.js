38fc5d6c2a37c414912c2df36dd09903e286be7a
Weng Fei Fung <weffung@ucdavis.edu>
2022-02-10 19:28:13 -0800

    Number string validator

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,18 @@
 
 let utilities = {
+    checkAllNumbers: (str) => {
+        str = "" + str;
+        if(str.length===0) return false;
+
+        let isNum = true;
+        for(let i = 0; i<str.length; i++) {
+            if(str[i]>'9' || str[i]<'0') {
+                isNum=false;
+                break;
+            }
+        } // for
+        return isNum;
+    }
 }
 
 let app = {
